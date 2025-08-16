49454e0aa0b5ec1040349238f013ed8709524794
Ердиас Талгатов <talgatov_y@ezdrav.kz>
2025-08-16 12:39:04 +0500

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/hackload/billetter_api/BilletterApiApplication.java
@@ -0,0 +1,13 @@
+package com.hackload.billetter_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilletterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilletterApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackload/billetter_api/BilletterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackload.billetter_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilletterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
