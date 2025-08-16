6c63eb4d9ed6651d479c03b9b5c426fbe35499f1
Ердиас Талгатов <talgatov_y@ezdrav.kz>
2025-08-16 16:20:39 +0500

    uuid to long

--- src/main/java/com/hackload/billetter_api/dto/booking/BookingDto.java
+++ src/main/java/com/hackload/billetter_api/dto/booking/BookingDto.java
@@ -3,11 +3,10 @@ package com.hackload.billetter_api.dto.booking;
 import lombok.Data;
 
 import java.util.List;
-import java.util.UUID;
 
 @Data
 public class BookingDto {
-    private UUID id;
-    private UUID eventId;
-    private List<UUID> seats; // только список id мест
+    private long id;
+    private long eventId;
+    private List<Long> seats;
 }
