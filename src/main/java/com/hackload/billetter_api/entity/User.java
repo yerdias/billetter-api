a0070ff9730262a929b66c0a15ad01632d9e7624
RakhatMM <rakhatmm@gmail.com>
2025-08-16 17:17:11 +0500

    Fix: use JPA @Id in User entity

--- src/main/java/com/hackload/billetter_api/entity/User.java
+++ src/main/java/com/hackload/billetter_api/entity/User.java
@@ -3,7 +3,7 @@ package com.hackload.billetter_api.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
+import jakarta.persistence.Id;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
